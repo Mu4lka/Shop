a97d9b9507d399f24e07ff8aace52187c3f9ba12
Mu4lka <shatnykhmax@mail.ru>
2024-12-21 04:58:00 +0700

    update config for dev tunnel

--- front/shop/src/services/apiClient.js
+++ front/shop/src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:5282',
+    baseURL: 'https://zd3rdtwk-5282.euw.devtunnels.ms', //заменить на нужный url http://localhost:5282
     headers: {
     'Content-Type': 'application/json',
   },
--- front/shop/vue.config.js
+++ front/shop/vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    https: false,
+    client: {
+      webSocketURL: {
+        protocol: 'wss',
+        hostname: 'zd3rdtwk-8080.euw.devtunnels.ms', //if use remote tunnels ports in vs code
+        port: 443,
+      },
+    },
+    allowedHosts: 'all', 
+  },
+});
